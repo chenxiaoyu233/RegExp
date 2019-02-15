e6e2cc5299663873364359cf808a7b13bee73aeb
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-02-15 16:37:53 +0800

    [new feature] add main function for test

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include "RegExp.h"
+#include "Common.h"
+
+void printsub(string str, int begin, int end) {
+	for (int i = begin; i <= end; i++)  {
+		printf("%c", str[i]);
+	}
+	puts("");
+}
+
+void test() {
+	string rexp = "\[ab\]\{2, 3\}";
+	string text = "abcababcdefabababefacb";
+	RegExp matcher(rexp);
+	auto ret = matcher.Match(text);
+	for (auto pr: ret) {
+		printsub(text, pr.first, pr.second);
+	}
+}
+
+int main() {
+	test();
+	return 0;
+}
